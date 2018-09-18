4cb3847189f50a44aa9be118bbfaba484d8d34d8
HC1058503505 <1058503505@qq.com>
2018-09-18 15:31:06 +0800

    完善books路由

--- routes/books.js
+++ routes/books.js
@@ -3,37 +3,50 @@ const router = express.Router()
 const mongoClient = require('mongodb').MongoClient
 const dbURL = 'mongodb://localhost:27017'
 
+function responseAction(collectionName,query,project,req, res, skipnum,limitnum) {
+	
+	if (typeof(skipnum) != 'number') {
+		res.send("请输入整数page")
+		res.end()
+		return
+	}
 
-router.get('/',function(req, res){
+	if (typeof(limitnum) != 'number') {
+		res.send("请输入整数limitnum")
+		res.end()
+	}
+
+	
 	mongoClient.connect(dbURL, {useNewUrlParser:true},function(error, db){
-		const meals = db.db('books')
-		const caipu_list = meals.collection('guwen')
-
-		caipu_list.find({}).project({'book_contents' : 0}).limit(10).toArray(function (error, docs){
+		const books = db.db('books')
+		const guwen = books.collection(collectionName)
+		console.log(project)
+		guwen.find(query).project(project).skip(skipnum * limitnum).limit(limitnum).toArray(function (error, docs){
 			res.send(docs)
 			res.end()
 			db.close()
 		})
 	})
+}
+
+router.get('/:page',function(req, res){
+   var query = {
+
+   }
+
+   var project = {'book_contents' : 0}
+   responseAction("guwen",query,project,req, res, parseInt(req.params.page),10)
 })
 
 
-router.get('/:book_id',function(req, res){
-	mongoClient.connect(dbURL, {useNewUrlParser:true},function(error, db){
-		const meals = db.db('books')
-		const caipu_list = meals.collection('guwen')
-		// console.log(req.params.book_id)
-		// js 字符串转换数字:https://www.cnblogs.com/carekee/articles/1729574.html
-		var query = {
-			'book_id' : parseInt(req.params.book_id)
-		}
-
-		caipu_list.find(query).toArray(function (error, docs){
-			res.send(docs)
-			res.end()
-			db.close()
-		})
-	})
+router.get('/book_id/:book_id',function(req, res){
+	// console.log(req.params.book_id)
+	// js 字符串转换数字:https://www.cnblogs.com/carekee/articles/1729574.html
+	var query = {
+		'book_id' : parseInt(req.params.book_id)
+	}
+	var project = {}
+	responseAction("guwen",query,project,req, res, 0, 0)
 })
 
 module.exports = router
